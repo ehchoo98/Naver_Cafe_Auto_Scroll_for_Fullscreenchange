97ec5ce47b813c8dd7bd1c7848083409402757df
ehchoo98 <35360856+ehchoo98@users.noreply.github.com>
2023-04-19 12:52:59 +0900

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,13 @@
-const article = document.querySelector("article");
+var scrolX = 0;
+var scrolY = 0;
+let FullScreen = false;
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+onscroll = () => {
+  scrolx = window.scrollX;
+  scroly = window.scrollY;
+  setTimeout(() => {
+    FullScreen = document.fullscreen
+  }, 1000);
+  alert('scroll! ${scrolx}, ${scroly}, fullscreen = ${FullScreen}') 
 }
+
