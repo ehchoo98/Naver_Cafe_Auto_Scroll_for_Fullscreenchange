1e0ec84f09ee154eb6107fee2d86cf59bab0dc60
ehchoo98 <35360856+ehchoo98@users.noreply.github.com>
2023-06-01 05:51:18 +0900

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,13 @@
-let docs = top.document.querySelector("iframe#cafe_main").contentWindow.document 
 let screenElement
-
 try{
-    docs.addEventListener("fullscreenchange", () => {
-        if(docs.fullscreenElement) {
-            screenElement = docs.fullscreenElement
-        }else{
-            screenElement.scrollIntoView({block:"center"})
-        }
-        setTimeout(() => {}, 100)
-    }, {passive: true});
-}catch(e){
-    
-}
+    if(window.name=="cafe_main"){
+        document.addEventListener("fullscreenchange", () => {
+            if(document.fullscreenElement) {
+                screenElement = document.fullscreenElement
+            }else{
+                screenElement.scrollIntoView({block:"center"})
+            }
+            setTimeout(() => {}, 100)
+        }, {passive: true});
+    }
+}catch(e){}
